bf0890a6552ac43bbbd818ac86a4eca507ac936f
BerkaySimsek1 <113512628+BerkaySimsek1@users.noreply.github.com>
2023-08-27 19:25:55 +0300

    completing poetry club exercise

---
+++ swift/poetry-club/Sources/PoetryClub/PoetryClub.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+func splitOnNewlines(_ poem: String) -> [String] {
+  return poem.components(separatedBy: "\n")
+}
+
+func firstLetter(_ line: String) -> Character {
+  return line.first ?? "_"
+}
+
+func capitalize(_ phrase: String) -> String {
+  return phrase.capitalized
+}
+
+func trimSentence(_ line: String) -> String {
+  return line.trimmingCharacters(in: .whitespacesAndNewlines)
+}
+
+func lastLetter(_ line: String) -> Character {
+  return line.last ?? "_"
+}
+
+func backDoorPassword(_ phrase: String) -> String {
+  return "\(phrase.capitalized), please"
+}
+
+func ithLetter(_ line: String, i: Int) -> Character {
+  guard let index = line.index(line.startIndex, offsetBy: i, limitedBy: line.endIndex) else {
+        return " "
+    }
+    return line[index]
+}
+
+func secretRoomPassword(_ phrase: String) -> String {
+  return "\(phrase.uppercased())!"
+}
