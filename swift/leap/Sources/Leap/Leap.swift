be8e7e41b95d2911397ed78517ebe502b1b387c5
BerkaySimsek1 <113512628+BerkaySimsek1@users.noreply.github.com>
2023-09-06 22:53:09 +0300

    completing leap exercise

---
+++ swift/leap/Sources/Leap/Leap.swift
@@ -0,0 +1,10 @@
+class Year {
+    let year: Int
+
+    init(calendarYear: Int) {
+        year = calendarYear
+    }
+    var isLeapYear: Bool {
+        return year % 4 == 0 && (!(year % 100 == 0) || year % 400 == 0) 
+    }
+}
